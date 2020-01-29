ad6a7b9a1655b0f77ca8b839c6a79294fc91d4d4
rockmenjack <rockmen1@gmail.com>
2020-01-29 14:08:08 +0800

    pass 07 test

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,12 +1,12 @@
 extern crate proc_macro;
 
-use proc_macro2::TokenStream;
+use proc_macro2::{TokenStream,Span};
 use quote::{format_ident, quote,quote_spanned};
 use syn::spanned::Spanned;
 use syn::{parse_macro_input, DeriveInput, Ident, Data, Fields, Field, Type, Type::{Path}, PathSegment, GenericArgument, MetaNameValue, Lit};
 use syn::PathArguments::{AngleBracketed};
 
-#[proc_macro_derive(Builder)]
+#[proc_macro_derive(Builder,attributes(builder))]
 pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
 
@@ -21,7 +21,7 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
 enum FieldKind<'a> {
     Mandatory(&'a Ident, &'a Type, String), // String: not set error text
     Optional(&'a Ident, &'a Type), // wraps Option inner type
-    Repeated(&'a Ident, &'a Type, String), // wraps Vec inner type, String: setter function name
+    Repeated(&'a Ident, &'a Type, Ident), // wraps Vec inner type, String: setter function name
 }
 
 fn parse_field_kind<'a>(field : &'a Field) -> Option<FieldKind<'a>> {
@@ -43,29 +43,28 @@ fn parse_field_kind<'a>(field : &'a Field) -> Option<FieldKind<'a>> {
                 [first, second, third] => { // std::option::Option
                     if first.ident == "std" 
                        && second.ident == "option" {
-                        if let (Some(ty), Some(ident)) = (get_inner_field_kind(third, "Option"),&field.ident) {
-                            return Some(FieldKind::Optional(ident, ty));
+                        if let (Some(inner_ty), Some(ident)) = (get_inner_field_kind(third, "Option"),&field.ident) {
+                            return Some(FieldKind::Optional(ident, inner_ty));
                         }
                     }
                 },
                 [first] => { // Option
-                    if let (Some(ty), Some(ident)) = (get_inner_field_kind(first, "Option"),&field.ident) {
-                        return Some(FieldKind::Optional(ident, ty));
+                    if let (Some(inner_ty), Some(ident)) = (get_inner_field_kind(first, "Option"),&field.ident) {
+                        return Some(FieldKind::Optional(ident, inner_ty));
                     }
                 }
                 _ => {}
             }
 
             if let Some(attr) = &field.attrs.iter().find(|x| x.path.is_ident("builder") ) {
-                if let Ok(MetaNameValue{
-                    path: path,
-                    eq_token: _,
-                    lit: Lit::Str(lit_str),
-                }) = attr.parse_args::<MetaNameValue>() {
-                    if Some(ident) = path.get_ident() {
-                        if ident == "each" {
-                            if let (Some(ident), Some(ty)) = (path.get_ident(), get_inner_field_kind(&field.ident.first))
-                            return FieldKind::Repeated(ident, )
+                if let Ok(MetaNameValue{ path, eq_token: _, lit: Lit::Str(lit_str)}) = attr.parse_args::<MetaNameValue>() {
+                    if path.is_ident("each") {
+                        if let Path(outter_type) = &field.ty {
+                            if let Some(segment) = outter_type.path.segments.first() {
+                                if let (Some(ident), Some(inner_ty)) = (&field.ident, get_inner_field_kind(segment, "Vec")) {
+                                    return Some(FieldKind::Repeated(ident,inner_ty, Ident::new(&lit_str.value(), Span::call_site())));
+                                }
+                            }
                         }
                     }
                 }
@@ -93,9 +92,9 @@ fn add_builder_type(ident : &Ident, data : &Data) -> TokenStream {
         let mut builder_optional_field_inner_types : Vec<&Type> = Vec::new();
         let mut builder_optional_field_idents: Vec<&Ident> = Vec::new();
 
-        let mut builder_repeated_field_types : Vec<&Type> = Vec::new();
+        let mut builder_repeated_field_inner_types : Vec<&Type> = Vec::new();
         let mut builder_repeated_field_idents: Vec<&Ident> = Vec::new();
-        let mut builder_repeated_field_func_name: Vec<String> = Vec::new();
+        let mut builder_repeated_field_func_names: Vec<Ident> = Vec::new();
 
         if let Fields::Named(named_fields) = &data_struct.fields {
             named_fields.named.iter()
@@ -111,6 +110,11 @@ fn add_builder_type(ident : &Ident, data : &Data) -> TokenStream {
                                 builder_optional_field_idents.push(ident);
                                 builder_optional_field_inner_types.push(inner_ty);
                             },
+                            FieldKind::Repeated(ident, inner_type,func_name) => {
+                                builder_repeated_field_idents.push(ident);
+                                builder_repeated_field_inner_types.push(inner_type);
+                                builder_repeated_field_func_names.push(func_name);
+                            }
                         }
                     }
                 });
@@ -118,17 +122,20 @@ fn add_builder_type(ident : &Ident, data : &Data) -> TokenStream {
 
         let builder_fields_def = [
             quote!{#(#builder_mandatory_field_idents : Option<#builder_mandatory_field_types>),*},
-            quote!{#(#builder_optional_field_idents : Option<#builder_optional_field_inner_types>),*}
+            quote!{#(#builder_optional_field_idents : Option<#builder_optional_field_inner_types>),*},
+            quote!{#(#builder_repeated_field_idents : Vec<#builder_repeated_field_inner_types>),*},
         ];
 
         let builder_create_def = [
             quote!{#(#builder_mandatory_field_idents : None),*},
             quote!{#(#builder_optional_field_idents : None),*},
+            quote!{#(#builder_repeated_field_idents : Vec::new()),*},
         ];
 
         let builder_build_def = [
             quote!{#(#builder_mandatory_field_idents : self.#builder_mandatory_field_idents.clone().ok_or(#builder_mandatory_field_not_set_errors)?),*},
             quote!{#(#builder_optional_field_idents : self.#builder_optional_field_idents.clone()),*},
+            quote!{#(#builder_repeated_field_idents : self.#builder_repeated_field_idents.clone()),*},
         ];
 
         quote!{
@@ -145,6 +152,12 @@ fn add_builder_type(ident : &Ident, data : &Data) -> TokenStream {
             }
 
             impl #builder_ident {
+                pub fn build(&self) -> Result<#ident, Box<dyn std::error::Error>> {
+                    Ok(#ident{
+                        #(#builder_build_def),*                        
+                    })
+                }
+
                 #(pub fn #builder_mandatory_field_idents(&mut self, v : #builder_mandatory_field_types) -> &mut Self {
                     self.#builder_mandatory_field_idents = Some(v);
                     self
@@ -155,11 +168,10 @@ fn add_builder_type(ident : &Ident, data : &Data) -> TokenStream {
                     self
                 })*
 
-                pub fn build(&self) -> Result<#ident, Box<dyn std::error::Error>> {
-                    Ok(#ident{
-                        #(#builder_build_def),*                        
-                    })
-                }
+                #(pub fn #builder_repeated_field_func_names(&mut self, v : #builder_repeated_field_inner_types) -> &mut Self {
+                    self.#builder_repeated_field_idents.push(v);
+                    self
+                })*
             }
         }
     } else {
