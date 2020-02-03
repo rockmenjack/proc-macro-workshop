1486a0170f28785d36c23e73972d640b669bcda8
rockmenjack <rockmen1@gmail.com>
2020-02-03 11:36:19 +0800

    pass debug 03

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,10 +1,10 @@
 extern crate proc_macro;
 
 use proc_macro2::{TokenStream};
-use syn::{parse_macro_input, DeriveInput, Ident, Data, DataStruct, Fields};
+use syn::{parse_macro_input, DeriveInput, Data, DataStruct, Fields,Attribute, Meta, MetaNameValue, Lit};
 use quote::{quote};
 
-#[proc_macro_derive(CustomDebug)]
+#[proc_macro_derive(CustomDebug, attributes(debug))]
 pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
 
@@ -14,26 +14,51 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     token_stream.into()
 }
 
-fn list_struct_fields(data : &Data) -> Vec<&Ident> {
+fn fields_debug_definition(data : &Data) -> Vec<TokenStream> {
     if let Data::Struct(DataStruct{fields: Fields::Named(fields), ..}) = data {
-        fields.named.iter().filter_map(|f| f.ident.as_ref()).collect()
-    } else {
-        Vec::new()
+        let extract_format = |attrs : &Vec<Attribute>| {
+            attrs.iter().find_map(|attr| {
+                if let Ok(Meta::NameValue(MetaNameValue{path, lit: Lit::Str(lit), ..})) = attr.parse_meta() {
+                    if path.is_ident("debug") {
+                        return Some(lit.value())
+                    }
+                }
+                None
+            })
+        };
+
+        return fields.named.iter()
+            .filter_map(|f| {
+                if let Some(ident) = &f.ident {
+                    let tokens = if let Some(custom_formatter) = extract_format(&f.attrs) {
+                        quote!{
+                            .field(stringify!(#ident), &format_args!(#custom_formatter, self.#ident))
+                        }
+                    } else {
+                        quote!{
+                            .field(stringify!(#ident), &self.#ident)
+                        }
+                    };
+
+                    Some(tokens)
+                } else {
+                    None
+                }
+            }).collect();
     }
+
+    Vec::new()
 }
 
 fn add_debug_impl(input : DeriveInput) -> TokenStream {
     let struct_name = &input.ident;
-    let struct_name_str = struct_name.to_string();
-    let struct_fields = list_struct_fields(&input.data);
-
-    let struct_fields_str = struct_fields.iter().map(|f| f.to_string());
+    let struct_fields_format = fields_debug_definition(&input.data);
     
     quote!{
         impl std::fmt::Debug for #struct_name {
             fn fmt(&self, fmt: &mut std::fmt::Formatter) -> std::fmt::Result {
-                fmt.debug_struct(#struct_name_str)
-                #(.field(#struct_fields_str, &self.#struct_fields))*
+                fmt.debug_struct(stringify!(#struct_name))
+                #(#struct_fields_format)*
                 .finish()
             }
         }
