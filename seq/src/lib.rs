25a7fc7f1928d80bae954bd9200be120c672c2fb
rockmenjack <rockmen1@gmail.com>
2020-02-11 13:15:49 +0800

    pass seq 01

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,10 +1,36 @@
 extern crate proc_macro;
 
-use proc_macro::TokenStream;
+use proc_macro2::TokenStream;
+use syn::parse::{Parse, ParseStream};
+use syn::{parse_macro_input, Result, Token, Ident, LitInt};
+use quote::{quote};
 
 #[proc_macro]
-pub fn seq(input: TokenStream) -> TokenStream {
-    let _ = input;
+pub fn seq(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let _ = parse_macro_input!(input as Seq);
 
-    unimplemented!()
+    (quote!{}).into()
+}
+
+// syn::Ident, Token![in], syn::LitInt, Token![..], syn::LitInt.
+struct Seq {
+    ident : Ident,
+    in_token : Token![in],
+    start : LitInt,
+    dot_dot : Token![..],
+    end : LitInt,
+    remainings : TokenStream,
+}
+
+impl Parse for Seq {
+    fn parse(input: ParseStream) -> Result<Self> {
+        Ok(Self {
+            ident : input.parse()?,
+            in_token : input.parse()?,
+            start : input.parse()?,
+            dot_dot : input.parse()?,
+            end : input.parse()?,
+            remainings : input.parse()?,
+        })
+    }
 }
